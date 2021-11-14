a11a26c14d46ac76caea93a36d65430a16ea408a
nacerhadjsaid <nacer.hadjsaid@gmail.com>
2021-11-14 17:06:27 -0500

    rest api added

---
+++ RestApi/src/test/java/rest/SingleEmployeeResources.java
@@ -0,0 +1,19 @@
+package rest;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SingleEmployeeResources {
+    @Test
+    public void callSingleEmployeeResource(){
+        Response response = given().when().get("http://info.venturepulse.org:8080/service-webapp/api/SingleEmployeeResources/590a4acd35522970c7956cdf").then().statusCode(200).extract().response();
+        String statusLine = response.getStatusLine();
+        int statusCode = response.getStatusCode();
+        String body = response.getBody().prettyPrint();
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        Assert.assertEquals(statusCode, 200);
+    }
+}
